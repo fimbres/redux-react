61e228f833f889c59a41b9052e6da8301c3e30f8
Isaac <ifimbres@uabc.edu.mx>
2022-07-17 14:52:03 -0700

    basic counter implementation with redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch({ type: 'INC' });
+  };
+  const decrement = () => {
+    dispatch({ type: 'DEC' });
+  };
+  const addValue = () => {
+    dispatch({ type: 'ADD', payload: 10 });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{counter}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addValue}>Add 10</button>
     </div>
   );
 }
